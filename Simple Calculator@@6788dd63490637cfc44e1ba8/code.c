7e502037f86c32ee25dd60c53142171a6ae4485c
Sunaina Sharma <sunaina04sharma@gmail.com>
2025-03-01 21:27:05 +0200

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,21 +1,21 @@
 // Your code here...
 #include <stdio.h>
 int main(){
-    float n1,n2;
+    int n1,n2;
     char c;
-    scanf("%f %f %c",&n1,&n2,&c);
+    scanf("%d %d %c",&n1,&n2,&c);
     switch (c){
         case '+':
-        printf("%.f",(n1+n2));
+        printf("%d",(n1+n2));
         break;
         case '-':
-        printf("%.f",(n1-n2));
+        printf("%d",(n1-n2));
         break;
         case '*':
         printf("%d",(n1*n2));
         break;
         case '/':
-        printf("%.2f",(n1/n2));
+        (n2==0)?printf("error"):printf("%d",(n1/n2));
         break;
     }
 
